2a121f86c3f499fc9c7a84e274de727cf122852f
unknown <chemagr25@gmail.com>
2023-04-18 01:47:12 -0600

    remove password input

--- src/components/forms/FormClient.vue
+++ src/components/forms/FormClient.vue
@@ -9,10 +9,7 @@
                 <v-text-field clearable persistent-hint hint="Nombres del técnico" class="shadow bg-gray-100 pb-2 rounded"
                     :rules="nameRules" v-model="name" label="Nombre(s)"></v-text-field>
             </v-col>
-            <v-col cols="12" sm="6" >
-                <v-text-field type="password" clearable persistent-hint hint="Nombres del técnico" class="shadow bg-gray-100 pb-2 rounded"
-                    :rules="nameRules" v-model="password" label="password"></v-text-field>
-            </v-col>
+          
             <v-col cols="12" sm="6" >
                 <v-text-field clearable persistent-hint hint="Apellidos del técnico"
                 :rules="nameRules" class="shadow bg-gray-100  pb-2 rounded" v-model="lastName" label="Apellidos"></v-text-field>
