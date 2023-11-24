3d107eaa433dfaddadf2043a9b05a0e91395b320
Jose Maria Garcia <chemagr25@gmail.com>
2023-11-24 07:40:20 -0600

    add minifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,62 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     VitePWA({
-      workbox: {
-        globPatterns: ['**/*.{jpg,png,json,html,js,css,svg,woff2,woff,ttf, eot,ico}'],
-        runtimeCaching:[{
-          urlPattern: ({url}) =>{
-            return url.pathname.startsWith('/api')
+      manifest: {
+        theme_color: "#6a35f6",
+        background_color: "#5c35f6",
+        display: "minimal-ui",
+        scope: "/",
+        start_url: "/",
+        name: "compuactual",
+        short_name: "capp",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
-          handler: 'CacheFirst' as const,
-          options:{
-            cacheName: 'api-cache',
-            cacheableResponse: {
-              statuses: [0,200]
-            }
-          }
-        }],
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-    })
+      workbox: {
+        globPatterns: [
+          "**/*.{jpg,png,json,html,js,css,svg,woff2,woff,ttf, eot,ico}",
+        ],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/api");
+            },
+            handler: "CacheFirst" as const,
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
- 
-})
+});
